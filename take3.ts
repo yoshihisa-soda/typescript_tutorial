41273b0d938871677691218a4d3897837f32c0b9
yoshihisa-soda <soda.06141031@gmail.com>
2021-02-04 23:36:15 +0900

    about how to array

--- take3.ts
+++ take3.ts
@@ -31,4 +31,55 @@ if (places.includes("小岩駅")) {
     // 見つかった
 }
 
+const others = [
+    "市民",
+    "都民",
+    "国民"
+]
+
+// ソート
+const numbers = [30, 1, 100];
+
+numbers.sort();
+numbers.sort((a, b) => a - b);
+
+const stations = [
+    {name: "池袋", users: 558623},
+    {name: "新宿", users: 775386},
+    {name: "渋谷", users: 366128},
+    {name: "東京", users: 462589}
+    ];
+    stations.sort((a, b) => a.users - b.users);
+
+const stations = [
+    {name: "大手町", lines: 5, yomi: "おおてまち"},
+    {name: "飯田橋", lines: 7, yomi: "いいだばし"},
+    {name: "永田町", lines: 5, yomi: "ながたちょう"},
+    ];
+
+// number, stringどちらでもこのような書き方が可能
+function cmpNum (a: number, b: number) {
+    return (a < b) ? -1 : (a == b) ? 0 : 1; 
+}
+function cmpStr(a: string, b: string) {
+    return (a < b) ? -1 : (a === b) ? 0 : 1;
+    }
+
+stations.sort((a, b) => {
+    const lineScore = cmpNum(a.lines, b.lines);
+    const yomiScore = cmpStr(a.yomi, b.yomi);
+    // わかりやすく 10 倍しているが、2 倍でも OK
+    return lineScore * 10 + yomiScore;
+})
+
+// *非破壊のソートはない
+
+// for of ループ
+// entries() メソッドは、出力ターゲットを ES2015 以上にしないと動作しない
+
+// let iterable = ["小金井", "小淵沢", "小矢部"];
+// for (const [i, value] of iterable.entries()) {
+//     console.log(i, value);
+// }
+//
 
