2ef7615c9b0de13c82e598d8b9c4ffc15fda6c4d
yoshihisa-soda <soda.06141031@gmail.com>
2021-02-08 23:19:59 +0900

    various of type

--- take5.ts
+++ take5.ts
@@ -65,3 +65,78 @@ const shibukawa: Twitter & Instagram = {
     twitterId: "@chomechome",
     instagramId: "chomechome"
 }
+
+const grid = new cheetahGrid.ListGrid({
+    parentElement: document.querySelector('#sample2'),
+    header: [
+        { field: 'number', caption: 'number', columnType: 'number',
+            style: {color: 'red'}},
+        { field: 'check', caption: 'check', columnType: 'check',
+            style: {
+                uncheckBgColor: '#FDD',
+                checkBgColor: 'rgb(255, 73, 72)'
+            }
+        }
+    ],
+})
+
+// チェックボックスのカラム用の設定
+type CheckStyle = {
+    uncheckBgColor: string;
+    checkBgColor: string;
+}
+
+type CheckColumn = {
+    columnType: 'check';
+    caption: string;
+    field: string;
+    style: CheckStyle;
+}
+
+// 数値用のカラム用の設定
+type NumberStyle = {
+    color: string;
+}
+
+type NumberColumn = {
+    columnType: 'number';
+    caption: string;
+    field: string;
+    style: NumberStyle;
+}
+
+// 両方の型を取り得る合併型を定義
+type Column = CheckColumn | NumberColumn;
+
+// 無事、エラーを出さずに過不足なく型付けできた
+const header: Column[] = [
+{ field: 'number', caption: 'number', columnType: 'number',
+  style: {color: 'red'}},
+{ field: 'check', caption: 'check', columnType: 'check',
+  style: {
+    uncheckBgColor: '#FDD',
+    checkBgColor: 'rgb(255, 73, 72)'
+  }
+}
+];
+
+// 短い型定義
+type Style = {
+    color?: string;
+    uncheckBgColor?: string;
+    checkBgColor?: string;
+}
+
+type Column = {
+    columnType: 'number' | 'check';
+    caption: string;
+    field: string;
+    style: string;
+}
+
+// 型ガード
+// TypeScriptでは、今まで見て来た通り、少し柔軟な型を許容
+// 数値型 or null
+// 数字型 or 文字列
+// オブジェクトの特定の属性 columnType -> 'check' という文字列の場合のみ属性が増える
+
